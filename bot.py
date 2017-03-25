c5d3a36a52f98b7ad0c0a5bc452fed30805e711d
helloitu <gui.gui.guilhermec@hotmail.com>
2017-03-25 09:00:51 -0300

    first upload

---
+++ bot.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+from __future__ import unicode_literals
+import os
+import logging
+from urllib.request import urlopen
+
+import youtube_dl
+from bs4 import BeautifulSoup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from sqlalchemy.orm import Session
+
+from credentials import ENGINE, TOKEN, CELULAR, IPOD
+from database import Backup
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+u = Updater(TOKEN)
+dp = u.dispatcher
+
+
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text="CraraBot <3 \n Usual Telegram bot, YOLO, no UTF-8 characters pls <3")
+
+
+def music(bot, update):
+    title, video_url = search(update.message.text)
+    session = Session(bind=ENGINE)
+    session.add(Backup(title=title, video_url=video_url))
+    session.commit()
+    session.close()
+    download(title, video_url)
+    if CELULAR == 0:
+        print("Telegram sender Disabled / Envio para o telegram desativado, talvez seja algo nas configuracoes")
+    else:
+        print("Telegram sender Enabled / Enviando para o telegram")
+        bot.sendAudio(update.message.chat_id,
+         audio=open(title.replace(' ','') + '.mp3', 'rb'),
+          title=title)
+
+    ipod()
+    os.remove(title.replace(' ','') + '.mp3')
+
+def ipod():
+    if IPOD == 0:
+        print("Ipod Upload Disabled / Upload para o ipod desativado")
+        print("----------------------------------------------------")
+        print("./end")
+    else:
+        print("Uploading on ipod / Fazendo upload para o ipod")
+        os.system("gnupod_addsong -m /media/vaniture/IPOD\ DE\ GUI/ *.mp3")
+        print ("Upload Concluido!")
+        print("----------------------------------------------------")
+        print("./end")
+
+def search(text):
+    query = '+'.join(text.lower().split())
+    url = 'https://www.youtube.com/results?search_query=' + query
+    content = urlopen(url).read()
+    soup = BeautifulSoup(content, 'html.parser')
+    tag = soup.find('a', {'rel': 'spf-prefetch'})
+    title = tag.text
+    video_url = 'https://www.youtube.com' + tag.get('href')
+    return title, video_url
+
+
+def download(title, video_url):
+    ydl_opts = {
+        'outtmpl': title.replace(' ','') + '.%(ext)s',
+        'format': 'bestaudio/best', 'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
+
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(MessageHandler([Filters.text], music))
+
+u.start_polling()
+u.idle()
---
+++ credentials.py
@@ -0,0 +1,6 @@
+from sqlalchemy import create_engine
+
+ENGINE = create_engine('sqlite:///music.db')
+TOKEN = 'TELEGRAM-BOT-API-Key aqui'
+CELULAR = 1 # 1 enabled 0 disabled
+IPOD = 0
