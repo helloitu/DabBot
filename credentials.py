6dc06ac7ae29c4f0e5275ff8162c45725eae8c6a
Guilherme César dos Santos <gui.gui.guicesar12@hotmail.com>
2017-03-28 19:20:35 -0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -4,4 +4,4 @@ ENGINE = create_engine('sqlite:///music.db')
 TOKEN = 'TELEGRAM BOT API KEY'
 CELULAR = 1 # 1 enabled 0 disabled
 IPOD = 0
-Ipod_Path = /media/vaniture/ipod #example 
+Ipod_Path = "/media/vaniture/ipod" #example 
