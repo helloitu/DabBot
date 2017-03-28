4970584eef145a4c387ee9aa470493dc2b9077a7
Guilherme César dos Santos <gui.gui.guicesar12@hotmail.com>
2017-03-28 19:12:34 -0300

    Adicionando variavel path

--- credentials.py
+++ credentials.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 
 ENGINE = create_engine('sqlite:///music.db')
-TOKEN = 'TELEGRAM-BOT-API-Key aqui'
+TOKEN = 'TELEGRAM BOT API KEY'
 CELULAR = 1 # 1 enabled 0 disabled
 IPOD = 0
+Ipod_Path = /media/vaniture/ipod #example 
